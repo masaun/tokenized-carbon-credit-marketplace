a75412816e965983e0dee84b2999b0808135ec70
masaun <masanoriuno@gmail.com>
2021-04-16 11:33:13 +0900

    Rename with openToPutOnSale() / cancelToPutOnSale()

--- smart-contract/contracts/GreenNFTTradable.sol
+++ smart-contract/contracts/GreenNFTTradable.sol
@@ -18,7 +18,7 @@ contract GreenNFTTradable {
     /**
      * @notice - Open to put on sale of carbon credits.
      */
-    function openSale(GreenNFTData _greenNFTData, GreenNFT greenNFT, uint256 _greenId) public {
+    function openToPutOnSale(GreenNFTData _greenNFTData, GreenNFT greenNFT, uint256 _greenId) public {
         GreenNFTData greenNFTData = _greenNFTData;
         greenNFTData.updateStatus(greenNFT, GreenNFTDataCommons.GreenNFTStatus.Sale);
     }
@@ -26,7 +26,7 @@ contract GreenNFTTradable {
     /**
      * @notice - Cancel to put on sale of carbon credits.
      */
-    function cancelSale(GreenNFTData _greenNFTData, GreenNFT greenNFT, uint256 _greenId) public {
+    function cancelToPutOnSale(GreenNFTData _greenNFTData, GreenNFT greenNFT, uint256 _greenId) public {
         GreenNFTData greenNFTData = _greenNFTData;
         greenNFTData.updateStatus(greenNFT, GreenNFTDataCommons.GreenNFTStatus.NotSale);
     }
