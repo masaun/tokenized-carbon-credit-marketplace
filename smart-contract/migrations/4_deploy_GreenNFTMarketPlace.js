d8679615db8f28e8d75672ea6fb5150692d93077
masaun <masanoriuno@gmail.com>
2021-04-20 09:06:45 +0900

    [Migration]: Add the CarbonCreditToken's address as a assigned-value to deployment of the GreenNFTMarketplace.sol

--- smart-contract/migrations/4_deploy_GreenNFTMarketPlace.js
+++ smart-contract/migrations/4_deploy_GreenNFTMarketPlace.js
@@ -1,8 +1,10 @@
 const GreenNFTMarketplace = artifacts.require("./GreenNFTMarketplace.sol");
 const GreenNFTData = artifacts.require("./GreenNFTData.sol");
+const CarbonCreditToken = artifacts.require("./CarbonCreditToken.sol");
 
 const _greenNFTData = GreenNFTData.address;
+const _carbonCreditToken = CarbonCreditToken.address;
 
 module.exports = async function(deployer, network, accounts) {
-    await deployer.deploy(GreenNFTMarketplace, _greenNFTData);
+    await deployer.deploy(GreenNFTMarketplace, _greenNFTData, _carbonCreditToken);
 };
